f396e91c0652f4802289b975031fe7746b46d633
Ricardo Ángel Del Ángel Dávila <A01411440@itesm.mx>
2017-09-07 18:09:17 -0500

    Node

---
+++ ED-RDelAngelD-A06/src/ed/rdelangeld/a06/EDRDelAngelDA06.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ed.rdelangeld.a06;
+
+/**
+ *
+ * @author A01411440
+ */
+public class EDRDelAngelDA06 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
